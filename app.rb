6cf5d1f7018bfceb0a122877694d90326c8859d9
ayaka9922 <ayk.24a3y@gmail.com>
2020-02-06 06:36:19 +0000

    completed 003

--- app.rb
+++ app.rb
@@ -5,9 +5,18 @@ require 'sinatra/reloader' if development?
 require 'net/http'
 require 'uri'
 
-get '/:keyword' do
+get '/' do
   base_url = 'http://wikipedia.simpleapi.net/api?output=html&keyword='
-  keyword = params[:keyword]
-  url = URI.parse(base_url+keyword)
-  Net::HTTP.get(url)
+  if params[:keyword]
+    keyword = URI.escape(params[:keyword])
+    url = URI.parse(base_url+keyword)
+    @result = Net::HTTP.get(url).force_encoding("utf-8")
+  end
+  if @result == ''
+    @result = params[:keyword] + 'はありません'
+   end
+  if keyword == ''
+    @result = '{keyword} がありません'
+  end
+erb :index
 end
